cc2ecd9aa165ec9dd89f8fcd328a2f35cf6eb4a1
Your Name <youremail@example.com>
2024-11-09 02:35:35 +0900

    ✨ 機能: Jestの設定ファイルを追加

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testMatch: ['**/*.test.ts', '**/*.test.tsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
